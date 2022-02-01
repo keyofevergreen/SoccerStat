976994125a5b47c9a2b3d0e11f46bfbff7b52884
evergreen <dany2zverev@gmail.com>
2022-02-01 17:11:49 +0300

    add BackTop component

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,7 +1,18 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
+import { BackTop } from 'antd';
 import Navigation from './components/Navigation';
 import styles from './styles.module.scss';
+
+const style = {
+  height: 40,
+  width: 40,
+  lineHeight: '40px',
+  borderRadius: 4,
+  backgroundColor: '#1088e9',
+  color: '#fff',
+  textAlign: 'center',
+  fontSize: 14,
+};
 
 const Index = ({ children }) => (
   <div>
@@ -9,6 +20,9 @@ const Index = ({ children }) => (
     <div className={styles.wrapper}>
       {children}
     </div>
+    <BackTop>
+      <div style={style}>UP</div>
+    </BackTop>
   </div>
 );
 
