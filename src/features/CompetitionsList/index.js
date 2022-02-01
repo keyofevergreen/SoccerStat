70b8dc4aa9c1cdc626839841730233551a028b4f
evergreen <dany2zverev@gmail.com>
2022-02-01 17:21:24 +0300

    added CompetitionsList's index.js

---
+++ src/features/CompetitionsList/index.js
@@ -0,0 +1,10 @@
+import competitionsReducer from './reducer';
+import competitionsService from './service';
+
+export * from './actions';
+export * from './hooks';
+
+export {
+  competitionsReducer,
+  competitionsService,
+};
