846ff549db14578e509be2a75f85edce0dc6cc20
evergreen <dany2zverev@gmail.com>
2022-02-05 15:06:37 +0300

    renamed Routes elements, added route to competition page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import 'antd/dist/antd.css';
 import { Routes, Navigate, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Index from './pages/Index';
-import Competition from './pages/Competitions';
+import Competitions from './pages/Competitions';
+import Competition from './pages/Competition';
 
 const App = () => (
   <Index>
     <Routes>
       <Route index element={<Home />} />
-      <Route exact path="competitions/*" element={<Competition />} />
-      <Route path="competitions/:search" element={<Competition />} />
+      <Route exact path="competitions/*" element={<Competitions />} />
+      <Route path="competitions/:search" element={<Competitions />} />
+      <Route path="competition/:competitionId" element={<Competition />} />
       <Route
         path="*"
         element={<Navigate to="/" />}
