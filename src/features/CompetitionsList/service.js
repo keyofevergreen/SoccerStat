f39155d76d961a7a80eab528eb42e2f0f898553a
evergreen <dany2zverev@gmail.com>
2022-02-05 15:13:39 +0300

    fetch refactoring

--- src/features/CompetitionsList/service.js
+++ src/features/CompetitionsList/service.js
@@ -1,6 +1,6 @@
 const competitionsService = (rest) => ({
   fetchCompetitions: async () => {
-    const response = await rest('https://api.football-data.org/v2/competitions', {
+    const response = await rest('https://api.football-data.org/v2/competitions?plan=TIER_ONE&areas=2077', {
       method: 'GET',
       headers: {
         'X-Auth-Token': process.env.REACT_APP_API_KEY,
