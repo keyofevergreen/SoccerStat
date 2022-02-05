cfc44166bdafeba26d6b2fc0cff0bb4ae81f8428
evergreen <dany2zverev@gmail.com>
2022-02-05 15:11:39 +0300

    added Competition component

---
+++ src/pages/Competition/index.jsx
@@ -0,0 +1,43 @@
+import { Helmet } from 'react-helmet';
+import { useParams } from 'react-router';
+import { Button } from 'antd';
+import CompetitionCalendar from '../../features/CompetitionCalendar/components/CompetitionCalendar';
+import { useCompetitionMatches } from '../../features/CompetitionCalendar';
+import styles from './styles.module.scss';
+
+const Competition = () => {
+  const { competitionId } = useParams();
+  const [loading, error, matches, competition] = useCompetitionMatches(competitionId);
+  const competitionFlag = JSON.parse(localStorage.currentCompetitionFlag);
+  return (
+    <div>
+      <Helmet>
+        <title>{`SoccerStat - ${competition.name ? competition.name : 'Loading...'}`}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="League Calendar page" />
+      </Helmet>
+      {!loading && !error && (
+        <>
+          <div className={styles['page-header-wrap']}>
+            <h1 className="page-header">{competition.name}</h1>
+            <div className={styles['page-description-wrap']}>
+              <img src={competitionFlag.url} alt={competitionFlag.alt} className={styles['flag-img']} />
+              <span className="page-description">{competition?.area?.name}</span>
+            </div>
+          </div>
+          <Button type="primary" block className={styles['button-link']}>
+            Show participating teams
+          </Button>
+        </>
+      )}
+      <CompetitionCalendar
+        loading={loading}
+        error={error}
+        matches={matches}
+        competition={competition}
+      />
+    </div>
+  );
+};
+
+export default Competition;
