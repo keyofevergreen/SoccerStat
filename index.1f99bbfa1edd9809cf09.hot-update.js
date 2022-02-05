e1ff523d0784e6b349f739141df0451e6bb27ad4
evergreen <dany2zverev@gmail.com>
2022-02-05 21:33:05 +0300

    Updates

---
+++ index.1f99bbfa1edd9809cf09.hot-update.js
@@ -0,0 +1,16 @@
+/*!
+ * banner:
+ * SoccerStat: 1.0.0
+ * ISC
+ */
+"use strict";
+self["webpackHotUpdateSoccerStat"]("index",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c7d34dc014b42664fcee")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=index.1f99bbfa1edd9809cf09.hot-update.js.map
