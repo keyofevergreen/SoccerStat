16bda3940b74561661005043f3fa10dd33a8fef4
evergreen <dany2zverev@gmail.com>
2022-02-05 15:10:43 +0300

    added CompetitionCalendar index.js

---
+++ src/features/CompetitionCalendar/index.js
@@ -0,0 +1,10 @@
+import competitionMatchesReducer from './reducer';
+import competitionMatchesService from './service';
+
+export * from './actions';
+export * from './hooks';
+
+export {
+  competitionMatchesReducer,
+  competitionMatchesService,
+};
