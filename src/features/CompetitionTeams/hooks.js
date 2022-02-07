7abdbe05228289d0fdf1af91da0ae2aee9904107
evergreen <dany2zverev@gmail.com>
2022-02-07 20:16:09 +0300

    added CompetitionsTeams hooks.js

---
+++ src/features/CompetitionTeams/hooks.js
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchCompetitionTeams } from './thunks';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useCompetitionTeams = (id) => {
+  const [competitionCode] = id.split('-');
+  const dispatch = useDispatch();
+  // eslint-disable-next-line object-curly-newline
+  const { teams, competition, error, loading } = useSelector((state) => state.competitionTeams);
+
+  useEffect(() => {
+    dispatch(fetchCompetitionTeams(competitionCode));
+  }, []);
+
+  return [loading, error, teams, competition];
+};
