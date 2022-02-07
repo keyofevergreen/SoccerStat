8d3d4855d5fd3d2a40cf77d495c14023951e3b42
evergreen <dany2zverev@gmail.com>
2022-02-07 20:24:13 +0300

    added competitionTeams and team reducers

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { isDevelopment } from './utils/environments';
 import { competitionsReducer } from './features/CompetitionsList';
 import { competitionMatchesReducer } from './features/CompetitionCalendar';
+import { competitionTeamsReducer } from './features/CompetitionTeams';
+import { teamReducer } from './features/TeamCalendar';
 
 const createStore = ({
   history,
@@ -12,6 +14,8 @@ const createStore = ({
     reducer: {
       competitions: competitionsReducer,
       competitionMatches: competitionMatchesReducer,
+      competitionTeams: competitionTeamsReducer,
+      team: teamReducer,
     },
     devTools: isDevelopment(),
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
