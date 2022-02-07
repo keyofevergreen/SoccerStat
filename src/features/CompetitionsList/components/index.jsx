8da188ab1e54f8d137f7c2a55b298dbb73f16eef
evergreen <dany2zverev@gmail.com>
2022-02-07 20:20:56 +0300

    added CompetitionList component

---
+++ src/features/CompetitionsList/components/index.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import CompetitionItem from '../../../components/CompetitionItem/index';
+import LimitErrorMessage from '../../../components/LimitErrorMessage';
+import LoadingSpin from '../../../components/LoadingSpin';
+import styles from './styles.module.scss';
+import NotFoundMessage from '../../../components/NotFoundMessage';
+
+const CompetitionList = ({ loading, error, competitions }) => (
+  <div className={styles['competitions-wrap']}>
+    {loading && <LoadingSpin />}
+    {error && <LimitErrorMessage />}
+    {!error && !loading && !competitions.length
+      ? <NotFoundMessage>Not found competition for this query</NotFoundMessage>
+      : competitions.map((comp) => (
+        // eslint-disable-next-line
+        <CompetitionItem comp={comp} fullMode={true} key={comp.id} />
+      ))}
+  </div>
+);
+
+CompetitionList.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  competitions: PropTypes.array.isRequired,
+};
+
+export default CompetitionList;
