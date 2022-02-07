85143138b691e475700faebab9e8d53f1066fc29
evergreen <dany2zverev@gmail.com>
2022-02-07 20:12:56 +0300

    refactoring createActions strings

--- src/features/CompetitionCalendar/actions.js
+++ src/features/CompetitionCalendar/actions.js
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const requestCompetitionMatches = createAction('League matches is fetching...');
+export const requestCompetitionMatches = createAction('Competition matches is fetching...');
 
-export const requestCompetitionMatchesSuccess = createAction('League matches has already fetched');
+export const requestCompetitionMatchesSuccess = createAction('Competition matches has already fetched');
 
-export const requestCompetitionMatchesError = createAction('League matches fetched with error');
+export const requestCompetitionMatchesError = createAction('Competition matches fetched with error');
