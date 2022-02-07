850bbfe403593249732db14abe15cc20b43b884c
evergreen <dany2zverev@gmail.com>
2022-02-07 20:24:02 +0300

    added competitionTeams and team services

--- src/services.js
+++ src/services.js
@@ -1,9 +1,13 @@
 import competitionsService from './features/CompetitionsList/service';
 import { competitionMatchesService } from './features/CompetitionCalendar';
+import { competitionTeamsService } from './features/CompetitionTeams';
+import { teamService } from './features/TeamCalendar';
 
 const createServices = (rest) => ({
   competitions: competitionsService(rest),
   competitionMatches: competitionMatchesService(rest),
+  competitionTeams: competitionTeamsService(rest),
+  team: teamService(rest),
 });
 
 export default createServices;
