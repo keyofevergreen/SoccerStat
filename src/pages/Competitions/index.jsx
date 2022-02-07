30ccf7415567e62db220f822a0ca5c5c5a7b5003
evergreen <dany2zverev@gmail.com>
2022-02-07 20:22:33 +0300

    used useSortedList hook

--- src/pages/Competitions/index.jsx
+++ src/pages/Competitions/index.jsx
@@ -1,11 +1,11 @@
 import { Helmet } from 'react-helmet';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
-import CompetitionsList from '../../features/CompetitionsList/components/CompetitionsList';
+import CompetitionsList from '../../features/CompetitionsList/components';
 import { useCompetitions } from '../../features/CompetitionsList';
 import MyInput from '../../components/Input';
 import '../../assets/styles/global.scss';
-import styles from './styles.module.scss';
+import { useSortedList } from '../../helpers/hooks';
 
 // eslint-disable-next-line
 const Competitions = () => {
@@ -13,9 +13,8 @@ const Competitions = () => {
   const navigate = useNavigate();
   const [loading, error, competitions] = useCompetitions();
   const [searchQuery, setSearchQuery] = useState(search || '');
-  const [sortedCompetitions, setSortedCompetitions] = useState(competitions);
+  const [sortedCompetitions] = useSortedList(competitions, searchQuery);
 
-  const getMatchesWithSearch = (string) => string.toLowerCase().includes(searchQuery.toLowerCase());
   const handleChange = (inputValue) => {
     setSearchQuery(inputValue);
   };
@@ -23,14 +22,6 @@ const Competitions = () => {
   useEffect(() => {
     navigate(`/competitions/${searchQuery}`);
   }, [searchQuery]);
-
-  useEffect(() => {
-    setSortedCompetitions(
-      competitions
-        .filter((comp) => getMatchesWithSearch(comp.name) || getMatchesWithSearch(comp.area.name)),
-    );
-  }, [competitions, searchQuery]);
-
   return (
     <>
       <Helmet>
@@ -39,9 +30,7 @@ const Competitions = () => {
         <meta name="description" content="Competition page" />
       </Helmet>
       <h1 className="page-header">European Football Competition</h1>
-      <div className={styles['input-wrap']}>
-        <MyInput placeholder="Find the league" value={searchQuery} handleChange={handleChange} />
-      </div>
+      <MyInput placeholder="Find the league" value={searchQuery} handleChange={handleChange} />
       <CompetitionsList loading={loading} error={error} competitions={sortedCompetitions} />
     </>
   );
